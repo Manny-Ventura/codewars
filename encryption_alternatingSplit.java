0f68a93bab922fcccb5d788ea051b064c162748d
Your name here <wow.mannyv@gmail.com>
2021-10-05 19:56:42 -0400

    Finished repeater.java and removed encryption problem that was incomplete.

--- encryption_alternatingSplit.java
+++ encryption_alternatingSplit.java
@@ -1,23 +1,11 @@
 public class Kata {
 
     public static String encrypt(final String text, final int n) {
-        int index = 0;
-        String finalString = "";
-        char odd = text.charAt(index);
-        index++;
-        char even = text.charAt(index);
-        while (n != 0){
-            finalString += String.valueOf(odd);
-            finalString += String.valueOf(even);
-
-            index++;
-            odd = text.charAt(index);
-            index++;
-            even = text.charAt(index);
-            n--;
+        private static String final = "";
+        if (n != 0){
+            encrypt(finalString, n - 1)
         }
-        return null;   
-    }
+        return final;
     
     public static String decrypt(final String encryptedText, final int n) {
         // Your code here
