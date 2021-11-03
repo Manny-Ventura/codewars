92eeb18ed1435d1bdb8dcf089c0bf9ff416667d2
Your name here <wow.mannyv@gmail.com>
2021-11-02 23:04:54 -0400

    Reorganized the entire repository to individual language and difficulty level. Excited to when I need to make a level 5 folder, good luck out there soldier!

--- app.ts
+++
@@ -1,9 +0,0 @@
-import {User} from "./User"
-
-function main(){
-    let manny = new User(22, "Manny", 420);
-    let annabelle = new User(24, "Annabelle", 69)
-    manny.info();
-    annabelle.info();
-} //end main
-main();
