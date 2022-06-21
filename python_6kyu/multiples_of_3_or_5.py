afb677d9ecbe075baa34d1a880e96820a030c738
Manny-Ventura <wow.mannyv@gmail.com>
2022-06-21 18:40:45 -0400

    Created multiples_of_3_or_5.py

---
+++ python_6kyu/multiples_of_3_or_5.py
@@ -0,0 +1,18 @@
+def solution(number):
+    bank_of_numbers = []
+    three_index = 3
+    five_index = 5
+    while three_index < number:
+        bank_of_numbers.append(three_index)
+        three_index += 3
+    while five_index < number:
+        bank_of_numbers.append(five_index)
+        five_index += 5
+    
+    total = 0
+    result = []
+    for num in bank_of_numbers:
+        if num not in result:
+            result.append(num)
+            total += num
+    return total
